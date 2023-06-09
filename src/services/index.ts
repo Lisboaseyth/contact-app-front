8098fec970cd20a02e27448c8c3592db257bd1b4
Pedro Lisboa <102534718+Lisboaseyth@users.noreply.github.com>
2023-06-09 12:11:26 -0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://contact-app-x6qr.onrender.com",
-    timeout: 10000
-})
-
-// export const api = axios.create({
-//     baseURL: "https://localhost:3000"
-//   });
+    baseURL: "http://localhost:3000"
+  });
